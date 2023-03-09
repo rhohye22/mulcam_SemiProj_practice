ca474498889037b02f8c521c90ebb1fdc1981367
rhohye22 <nhw666@gmail.com>
2023-03-10 02:22:09 +0900

    Bizidcheck 기능추가

--- src/main/java/mul/cam/a/member/BizMemberController.java
+++ src/main/java/mul/cam/a/member/BizMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import mul.cam.a.dto.BizMemberDto;
 
@@ -28,4 +29,19 @@ public class BizMemberController {
 		model.addAttribute("message", message);
 		return "message";
 	}
+	
+	@ResponseBody
+	@RequestMapping(value = "Bizidcheck.do", method = RequestMethod.POST)
+	public String idcheck(String id) {
+//		System.out.println("MemberController idcheck " + new Date());
+		
+		boolean isS = service.idCheck(id);
+		if(isS) {
+			return "NO";		// id가 있음
+		}
+		return "YES";			// id가 없음
+	}	
+	
+	
+	
 }
